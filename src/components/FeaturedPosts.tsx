56ceeaab073c2b8e71435e86183bcfc17dced95b
rlawnahd <rlawnahd123@naver.com>
2023-09-06 13:02:04 +0900

    css 수정

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -6,7 +6,7 @@ export default async function FeaturedPosts() {
     const posts = await getFeaturedPosts();
     return (
         <section>
-            <h2>Featured Posts</h2>
+            <h2 className="text-2xl font-bold">Featured Posts</h2>
             <PostsGrid posts={posts} />
         </section>
     );
