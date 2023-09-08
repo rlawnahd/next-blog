7c8a9c3e489a7b1a5895a7cf3f076ce5b7135805
rlawnahd <rlawnahd123@naver.com>
2023-09-08 09:08:00 +0900

    feat: post페이지 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,16 +1,34 @@
+import MarkdownViewer from '@/components/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
 import React from 'react';
+import { SlCalender } from 'react-icons/sl';
 type Props = {
     params: {
         slug: string;
     };
 };
 export default async function PostPage({ params: { slug } }: Props) {
-    const post = await getPostData(slug);
+    const { title, description, date, path, content } = await getPostData(slug);
     return (
-        <>
-            <h1>{post.title}</h1>
-            <pre>{post.content}</pre>
-        </>
+        <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+            <Image
+                className="w-full h-1/5 max-h-[500px]"
+                src={`/images/posts/${path}.png`}
+                alt={title}
+                width={760}
+                height={420}
+            />
+            <section className="flex flex-col p-4">
+                <div className="flex items-center self-end text-sky-600">
+                    <SlCalender />
+                    <p className="font-semibold ml-2">{date.toString()}</p>
+                </div>
+                <h1 className="text-4xl font-bold">{title}</h1>
+                <p className="text-xl font-bold">{description}</p>
+                <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
+                <MarkdownViewer content={content} />
+            </section>
+        </article>
     );
 }
--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 export default function MarkdownViewer({ content }: { content: string }) {
-    return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+    return (
+        <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+            {content}
+        </ReactMarkdown>
+    );
 }
