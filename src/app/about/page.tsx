2cbfe81af703100805d82b3f370aff37a6b11c44
rlawnahd <rlawnahd123@naver.com>
2023-09-12 13:43:45 +0900

    wip:contact page email form 작업중

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,26 @@
+import Hero from '@/components/Hero';
 import React from 'react';
 
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2';
 export default function AboutPage() {
-    return <div>AboutPage</div>;
+    return (
+        <>
+            <Hero />
+            <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+                <h2 className={TITLE_CLASS}>Who Am I?</h2>
+                <p>
+                    프론트엔드 개발자 <br />
+                    웹앱어플리케이션을 만드는중..
+                </p>
+                <h2 className={TITLE_CLASS}>Career</h2>
+                <p>구글 (-Now)</p>
+                <p>레드펜 소프트 (-2023)</p>
+                <h2 className={TITLE_CLASS}>Skills</h2>
+                <p>
+                    Javascript, Typescript, React.js, Next.js <br />
+                    Node.js, Git
+                </p>
+            </section>
+        </>
+    );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,31 @@
+import ContactForm from '@/components/ContactForm';
 import React from 'react';
-
+import { AiFillGithub, AiFillYoutube, AiFillInstagram } from 'react-icons/ai';
+const LINKS = [
+    { icon: <AiFillGithub />, url: '' },
+    { icon: <AiFillInstagram />, url: '' },
+    { icon: <AiFillYoutube />, url: '' },
+];
 export default function ContactPage() {
-    return <div>ContactPage</div>;
+    return (
+        <section className="flex flex-col items-center ">
+            <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+            <p>rlawnahd123@naver.com</p>
+            <ul className="flex gap-4 my-2">
+                {LINKS.map((link, index) => (
+                    <a
+                        className="text-5xl hover:text-yellow-400"
+                        key={index}
+                        href={link.url}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {link.icon}
+                    </a>
+                ))}
+            </ul>
+            <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+            <ContactForm />
+        </section>
+    );
 }
