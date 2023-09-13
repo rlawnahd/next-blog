96d2c51af59a0e771e44b98e396d46ed3cd8cd9c
rlawnahd <rlawnahd123@naver.com>
2023-09-13 17:37:17 +0900

    test: nodemailer 이메일 전송 테스트 작업중

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { ChangeEvent, FormEvent, useState } from 'react';
+import Banner, { BannerData } from './Banner';
 type Form = {
     from: string;
     subject: string;
@@ -7,6 +8,7 @@ type Form = {
 };
 export default function ContactForm() {
     const [form, setForm] = useState<Form>({ from: '', subject: '', message: '' });
+    const [banner, setBanner] = useState<BannerData | null>(null);
     const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setForm((prev) => ({ ...prev, [name]: value }));
@@ -14,13 +16,25 @@ export default function ContactForm() {
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(form);
+        setBanner({ message: '성공했어', state: 'success' });
+        setTimeout(() => {
+            setBanner(null);
+        }, 3000);
     };
     return (
-        <>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="from">Your Email</label>
+        <section className="w-full max-w-md">
+            {banner && <Banner banner={banner} />}
+            <form
+                className="w-full  flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white"
+                onSubmit={onSubmit}
+            >
+                <label htmlFor="from" className="font-semibold">
+                    Your Email
+                </label>
                 <input type="mail" id="from" name="from" required autoFocus value={form.from} onChange={onChange} />
-                <label htmlFor="subject">Subject</label>
+                <label htmlFor="subject" className="font-semibold">
+                    Subject
+                </label>
                 <input
                     type="text"
                     id="subject"
@@ -30,7 +44,9 @@ export default function ContactForm() {
                     value={form.subject}
                     onChange={onChange}
                 />
-                <label htmlFor="message">Message</label>
+                <label htmlFor="message" className="font-semibold">
+                    Message
+                </label>
                 <textarea
                     rows={10}
                     id="message"
@@ -39,9 +55,10 @@ export default function ContactForm() {
                     autoFocus
                     value={form.message}
                     onChange={onChange}
+                    className="text-black"
                 />
-                <button>Submit</button>
+                <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">Submit</button>
             </form>
-        </>
+        </section>
     );
 }
